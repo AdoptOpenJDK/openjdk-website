f4dfcb356a4863d49b56669c4fd45bbfd4e2cf94
Enrique Lacal Bereslawski <enriquelacalbereslawski@enriques-mbp.hursley.uk.ibm.com>
2017-07-26 13:52:20 +0100

    Add Installer to Home Page

--- src/js/index.js
+++ src/js/index.js
@@ -52,7 +52,7 @@ function buildHomepageHTML(releasesJson) {
   });
 
   var OS = detectOS(); // set a variable as an object containing all information about the user's OS (from the global.js 'platforms' array)
-  var matchingBinary = null;
+  var matchingFile = null;
 
   // if the OS has been detected...
   if(OS) {
@@ -66,13 +66,24 @@ function buildHomepageHTML(releasesJson) {
 
         // secondly, check if the file has the expected file extension for that platform...
         // (this filters out all non-binary attachments, e.g. SHA checksums - these contain the platform name, but are not binaries)
-        var thisBinaryExtension = getBinaryExt(thisPlatform); // get the file extension associated with this platform
-        if(uppercaseFilename.indexOf(thisBinaryExtension.toUpperCase()) >= 0) {
-          var uppercaseOSname = OS.searchableName.toUpperCase();
+        var thisBinaryExtension = getBinaryExt(thisPlatform); // get the binary extension associated with this platform
+        var thisInstallerExtension = getInstallerExt(thisPlatform); // get the installer extension associated with this platform
+        if(matchingFile == null){
+          if(uppercaseFilename.indexOf(thisInstallerExtension.toUpperCase()) >= 0) {
+            var uppercaseOSname = OS.searchableName.toUpperCase();
 
-          // thirdly, check if the user's OS searchableName string matches part of this binary's name (e.g. ...X64_LINUX...)
-          if(uppercaseFilename.indexOf(uppercaseOSname) >= 0) {
-            matchingBinary = eachAsset; // set the matchingBinary variable to the object containing this binary
+            // thirdly, check if the user's OS searchableName string matches part of this binary's name (e.g. ...X64_LINUX...)
+            if(uppercaseFilename.indexOf(uppercaseOSname) >= 0) {
+              matchingFile = eachAsset; // set the matchingFile variable to the object containing this binary
+            }
+          }
+          else if(uppercaseFilename.indexOf(thisBinaryExtension.toUpperCase()) >= 0) {
+            var uppercaseOSname = OS.searchableName.toUpperCase();
+
+            // thirdly, check if the user's OS searchableName string matches part of this binary's name (e.g. ...X64_LINUX...)
+            if(uppercaseFilename.indexOf(uppercaseOSname) >= 0) {
+              matchingFile = eachAsset; // set the matchingFile variable to the object containing this binary
+            }
           }
         }
       }
@@ -80,27 +91,27 @@ function buildHomepageHTML(releasesJson) {
   }
 
   // if there IS a matching binary for the user's OS...
-  if(matchingBinary) {
-    dlLatest.href = matchingBinary.browser_download_url; // set the main download button's link to be the binary's download url
+  if(matchingFile) {
+    dlLatest.href = matchingFile.browser_download_url; // set the main download button's link to be the binary's download url
     dlText.innerHTML = ('Download for <var platform-name>' + OS.officialName + '</var>'); // set the text to be OS-specific, using the full OS name.
-    var thisBinarySize = Math.floor((matchingBinary.size)/1024/1024);
+    var thisBinarySize = Math.floor((matchingFile.size)/1024/1024);
     dlVersionText.innerHTML += (' - ' + thisBinarySize + ' MB');
-    if(matchingBinary.jck === true) {
+    if(matchingFile.jck === true) {
       document.getElementById('jck-approved-tick').classList.remove('hide');
       setTickLink();
     }
   }
   // if there is NOT a matching binary for the user's OS...
   else {
-    dlOther.className += ' hide'; // hide the 'Other platforms' button
-    dlIcon.className += ' hide'; // hide the download icon on the main button, to make it look less like you're going to get a download immediately
-    dlIcon2.className = dlIcon2.className.replace( /(?:^|\s)hide(?!\S)/g , '' ); // un-hide an arrow-right icon to show instead
+    dlOther.classList.add('hide'); // hide the 'Other platforms' button
+    dlIcon.classList.add('hide'); // hide the download icon on the main button, to make it look less like you're going to get a download immediately
+    dlIcon2.classList.remove('hide'); // un-hide an arrow-right icon to show instead
     dlText.innerHTML = ('Downloads'); // change the text to be generic: 'Downloads'.
     dlLatest.href = './releases.html'; // set the main download button's link to the latest builds page for all platforms.
   }
 
   // remove the loading dots, and make all buttons visible, with animated fade-in
-  loading.innerHTML = '';
+  loading.classList.add('hide');
   dlLatest.className = dlLatest.className.replace( /(?:^|\s)invisible(?!\S)/g , ' animated ' );
   dlOther.className = dlOther.className.replace( /(?:^|\s)invisible(?!\S)/g , ' animated ' );
   dlArchive.className = dlArchive.className.replace( /(?:^|\s)invisible(?!\S)/g , ' animated ' );
