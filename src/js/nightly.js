1c9ea0c71fe4b3cdbe94b64c6194a985904baebd
John Oliver <johno@insightfullogic.com>
2018-07-12 12:30:24 +0100

    Revert "WIP - Read new repositories (#266)" (#267)
    This reverts commit 6b23dbd3b1adb1012173877f832412bbbcb3aadb.

--- src/js/nightly.js
+++ src/js/nightly.js
@@ -17,10 +17,10 @@ function onNightlyLoad() {
   setDatePicker();
   populateNightly(); // run the function to populate the table on the Nightly page.
 
-  numberpicker.onchange = function () {
+  numberpicker.onchange = function(){
     setTableRange();
   };
-  datepicker.onchange = function () {
+  datepicker.onchange = function(){
     setTableRange();
   };
 }
@@ -34,117 +34,79 @@ function setDatePicker() {
   datepicker.value = today;
 }
 
-/* eslint-disable no-undef */
 function populateNightly() {
-  loadPlatformsThenData(function () {
+  loadPlatformsThenData(function() {
 
-    var handleResponse = function (response, oldRepo) {
 
-      if (response == null) {
-        return false;
+    var repoName = (variant + '-nightly');
+
+    loadJSON(repoName, 'nightly', function(response) {
+
+      function checkIfProduction(x) { // used by the array filter method below.
+        return x.prerelease === false && x.assets[0];
       }
 
       // Step 1: create a JSON from the XmlHttpRequest response
-      var releasesJson = response;
-
+      // Step 2: filter out all releases from this JSON that are marked as 'pre-release' in GitHub.
+      var releasesJson = JSON.parse(response).filter(checkIfProduction);
 
       // if there are releases...
       if (typeof releasesJson[0] !== 'undefined') {
-        var files = getFiles(releasesJson, oldRepo);
-
-        if (files.length === 0) {
-          return false;
-        }
-        buildNightlyHTML(files, oldRepo);
+        buildNightlyHTML(releasesJson);
       } else { // if there are no releases...
         // report an error
         errorContainer.innerHTML = '<p>Error... no releases have been found!</p>';
         loading.innerHTML = ''; // remove the loading dots
       }
-
-      return true;
-    };
-
-    loadAssetInfo(variant, 'nightly', 'nightly', handleResponse);
+    });
   });
+
 }
 
-/* eslint-disable no-undef */
-function getFiles(releasesJson, oldRepo) {
-  var assets = [];
+function buildNightlyHTML(releasesJson) {
+  tableHead.innerHTML = ('<tr id=\'table-header\'><th>Release</th><th>Date</th><th>Platform</th><th>Binary</th><th>Checksum</th></tr>');
+  var NIGHTLYARRAY = [];
 
   // for each release...
-  releasesJson.forEach(function (eachRelease) {
+  releasesJson.forEach(function(eachRelease) {
 
     // create an array of the details for each binary that is attached to a release
     var assetArray = [];
-    eachRelease.assets.forEach(function (each) {
+    eachRelease.assets.forEach(function(each) {
       assetArray.push(each);
     });
 
-    assetArray.forEach(function (eachAsset) {
+    // build rows with the array of binaries...
+    assetArray.forEach(function(eachAsset) {  // for each file attached to this release...
       var NIGHTLYOBJECT = new Object();
       var nameOfFile = (eachAsset.name);
       var uppercaseFilename = nameOfFile.toUpperCase(); // make the name of the file uppercase
       NIGHTLYOBJECT.thisPlatform = getSearchableName(uppercaseFilename); // get the searchableName, e.g. MAC or X64_LINUX.
-      var isArchive = new RegExp('(.tar.gz|.zip)$').test(nameOfFile);
-      var variantMatches = new RegExp(jvmVariant).test(eachAsset.name);
-
-      var correctFile = oldRepo || variantMatches && isArchive;
 
       // firstly, check if the platform name is recognised...
-      if (correctFile && NIGHTLYOBJECT.thisPlatform) {
-        assets.push({
-          release: eachRelease,
-          asset: eachAsset
-        })
+      if(NIGHTLYOBJECT.thisPlatform) {
+
+        // secondly, check if the file has the expected file extension for that platform...
+        // (this filters out all non-binary attachments, e.g. SHA checksums - these contain the platform name, but are not binaries)
+        NIGHTLYOBJECT.thisBinaryExtension = getBinaryExt(NIGHTLYOBJECT.thisPlatform); // get the file extension associated with this platform
+        if(uppercaseFilename.indexOf(NIGHTLYOBJECT.thisBinaryExtension.toUpperCase()) >= 0) {
+
+          // set values ready to be injected into the HTML
+          var publishedAt = eachRelease.published_at;
+          NIGHTLYOBJECT.thisReleaseName = eachRelease.name.slice(0, 12);
+          NIGHTLYOBJECT.thisReleaseDay = moment(publishedAt).format('D');
+          NIGHTLYOBJECT.thisReleaseMonth = moment(publishedAt).format('MMMM');
+          NIGHTLYOBJECT.thisReleaseYear = moment(publishedAt).format('YYYY');
+          NIGHTLYOBJECT.thisGitLink = ('https://github.com/AdoptOpenJDK/' + variant + '-nightly/releases/tag/' + eachRelease.name);
+          NIGHTLYOBJECT.thisOfficialName = getOfficialName(NIGHTLYOBJECT.thisPlatform);
+          NIGHTLYOBJECT.thisBinaryLink = (eachAsset.browser_download_url);
+          NIGHTLYOBJECT.thisBinarySize = Math.floor((eachAsset.size)/1024/1024);
+          NIGHTLYOBJECT.thisChecksumLink = (eachAsset.browser_download_url).replace(NIGHTLYOBJECT.thisBinaryExtension, '.sha256.txt');
+
+          NIGHTLYARRAY.push(NIGHTLYOBJECT);
+        }
       }
     });
-  });
-
-  return assets;
-}
-
-function buildNightlyHTML(files, oldRepo) {
-  tableHead.innerHTML = ('<tr id=\'table-header\'><th>Platform</th><th>Type</th></th><th>Date</th><th>Binary</th><th>Checksum</th></tr>');
-  var NIGHTLYARRAY = [];
-
-  // for each release...
-  files.forEach(function (file) {  // for each file attached to this release...
-
-    var eachAsset = file.asset;
-    var eachRelease = file.release;
-
-    var NIGHTLYOBJECT = new Object();
-    var nameOfFile = (eachAsset.name);
-    var uppercaseFilename = nameOfFile.toUpperCase(); // make the name of the file uppercase
-    NIGHTLYOBJECT.thisPlatform = getSearchableName(uppercaseFilename); // get the searchableName, e.g. MAC or X64_LINUX.
-    var type = nameOfFile.includes('-jre') ? 'jre' : 'jdk';
-
-    // secondly, check if the file has the expected file extension for that platform...
-    // (this filters out all non-binary attachments, e.g. SHA checksums - these contain the platform name, but are not binaries)
-    NIGHTLYOBJECT.thisBinaryExtension = getBinaryExt(NIGHTLYOBJECT.thisPlatform); // get the file extension associated with this platform
-    if (uppercaseFilename.indexOf(NIGHTLYOBJECT.thisBinaryExtension.toUpperCase()) >= 0) {
-
-      // set values ready to be injected into the HTML
-      var publishedAt = eachRelease.published_at;
-      NIGHTLYOBJECT.thisReleaseName = eachRelease.name.slice(0, 12);
-      NIGHTLYOBJECT.thisType = type;
-      NIGHTLYOBJECT.thisReleaseDay = moment(publishedAt).format('D');
-      NIGHTLYOBJECT.thisReleaseMonth = moment(publishedAt).format('MMMM');
-      NIGHTLYOBJECT.thisReleaseYear = moment(publishedAt).format('YYYY');
-      NIGHTLYOBJECT.thisGitLink = ('https://github.com/AdoptOpenJDK/' + variant + '-nightly/releases/tag/' + eachRelease.name);
-      NIGHTLYOBJECT.thisOfficialName = getOfficialName(NIGHTLYOBJECT.thisPlatform);
-      NIGHTLYOBJECT.thisBinaryLink = (eachAsset.browser_download_url);
-      NIGHTLYOBJECT.thisBinarySize = Math.floor((eachAsset.size) / 1024 / 1024);
-      if(oldRepo) {
-        NIGHTLYOBJECT.thisChecksumLink = (eachAsset.browser_download_url).replace(NIGHTLYOBJECT.thisBinaryExtension, '.sha256.txt');
-      } else {
-        NIGHTLYOBJECT.thisChecksumLink = eachAsset.browser_download_url + '.sha256.txt';
-      }
-
-      NIGHTLYARRAY.push(NIGHTLYOBJECT);
-    }
   });
 
   NIGHTLYDATA.htmlTemplate = NIGHTLYARRAY;
@@ -156,7 +118,7 @@ function buildNightlyHTML(files, oldRepo) {
   loading.innerHTML = ''; // remove the loading dots
 
   // show the table, with animated fade-in
-  nightlyList.className = nightlyList.className.replace(/(?:^|\s)hide(?!\S)/g, ' animated fadeIn ');
+  nightlyList.className = nightlyList.className.replace( /(?:^|\s)hide(?!\S)/g , ' animated fadeIn ' );
   setTableRange();
 
   // if the table has a scroll bar, show text describing how to horizontally scroll
@@ -164,7 +126,7 @@ function buildNightlyHTML(files, oldRepo) {
   var tableDisplayWidth = document.getElementById('nightly-list').clientWidth;
   var tableScrollWidth = document.getElementById('nightly-list').scrollWidth;
   if (tableDisplayWidth != tableScrollWidth) {
-    scrollText.className = scrollText.className.replace(/(?:^|\s)hide(?!\S)/g, '');
+    scrollText.className = scrollText.className.replace( /(?:^|\s)hide(?!\S)/g , '' );
   }
 }
 
@@ -177,7 +139,7 @@ function setTableRange() {
     var thisDate = rows[i].getElementsByClassName('nightly-release-date')[0].innerHTML;
     var thisDateMoment = moment(thisDate, 'D MMMM YYYY').format();
     var isAfter = moment(thisDateMoment).isAfter(selectedDate);
-    if (isAfter === true || visibleRows >= numberpicker.value) {
+    if(isAfter === true || visibleRows >= numberpicker.value) {
       rows[i].classList.add('hide');
     }
     else {
@@ -192,14 +154,14 @@ function setTableRange() {
 function setSearchLogic() {
   // logic for the realtime search box...
   var $rows = $('#nightly-table tr');
-  $('#search').keyup(function () {
+  $('#search').keyup(function() {
     var val = '^(?=.*' + $.trim($(this).val()).split(/\s+/).join(')(?=.*') + ').*$',
-      reg = RegExp(val, 'i'),
-      text;
+        reg = RegExp(val, 'i'),
+        text;
 
-    $rows.show().filter(function () {
-      text = $(this).text().replace(/\s+/g, ' ');
-      return !reg.test(text);
+    $rows.show().filter(function() {
+        text = $(this).text().replace(/\s+/g, ' ');
+        return !reg.test(text);
     }).hide();
 
     checkSearchResultsExist();
@@ -208,7 +170,7 @@ function setSearchLogic() {
 
 function checkSearchResultsExist() {
   var numOfVisibleRows = $('#nightly-table').find('tr:visible').length;
-  if (numOfVisibleRows == 0) {
+  if(numOfVisibleRows == 0){
     tableContainer.style.visibility = 'hidden';
     searchError.className = '';
   }
