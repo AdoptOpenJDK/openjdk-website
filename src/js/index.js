56b364473a7f156164d121c16f9ef87cf5b74424
David Dunn <davdunn2@uk.ibm.com>
2018-10-26 17:45:02 +0100

    Fix failing check & white space issues

--- src/js/index.js
+++ src/js/index.js
@@ -14,11 +14,6 @@ function onIndexLoad() {
 }
 /* eslint-enable no-unused-vars */
 
-function togglePopup() {
-  var popup = document.getElementById("jvm-popup");
-  popup.classList.toggle("show");
-}
-
 // INDEX PAGE FUNCTIONS
 function removeRadioButtons() {
   var buttons = document.getElementsByClassName('btn-label');
