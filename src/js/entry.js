ed64ef209abf6e3182f2ba0ecf5543bb5a3c4c85
Greg Parris <greg.parris@gmail.com>
2018-11-25 12:09:42 -0600

    Fix extracting the page name
    There might be a context root prefix (e.g. '/375' during staging)

--- src/js/entry.js
+++ src/js/entry.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // '/index.html' --> 'index'
   // NOTE: Browserify requires strings in `require()`, so this is intentionally more explicit than
   // it normally would be.
-  switch(window.location.pathname.substring(1).replace(/\.html$/i, '')) {
+  switch(window.location.pathname.split('/').pop().replace(/\.html$/i, '')) {
     case '':
     case 'index':
       return require('./index').load();
