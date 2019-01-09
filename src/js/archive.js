6cbb8584afd40e20ccb23b926685c84e530100ef
Greg <supahgreg@users.noreply.github.com>
2019-01-09 10:53:46 -0600

    Rework Archives to support Installer+JDK+JRE (#383)

--- src/js/archive.js
+++ src/js/archive.js
@@ -1,5 +1,5 @@
 const {findPlatform, getBinaryExt, getInstallerExt, getOfficialName, getPlatformOrder,
-  loadAssetInfo, orderPlatforms, setRadioSelectors, setTickLink} = require('./common');
+  loadAssetInfo, setRadioSelectors, sortByProperty} = require('./common');
 const {jvmVariant, variant} = require('./common');
 
 const loading = document.getElementById('loading');
@@ -18,128 +18,76 @@ module.exports.load = () => {
   });
 }
 
-function buildArchiveHTML(releases, jckJSON = {}) {
-  const RELEASEARRAY = [];
+function buildArchiveHTML(aReleases) {
+  const releases = [];
 
-  for (let i = 0; i<releases.length; i++) {
-    const ASSETARRAY = [];
-    const RELEASEOBJECT = {};
-    const eachRelease = releases[i];
+  aReleases.forEach(aRelease => {
+    const publishedAt = moment(aRelease.timestamp);
 
-    // set values for this release, ready to inject into HTML
-    const publishedAt = moment(eachRelease.timestamp);
-    RELEASEOBJECT.thisReleaseName = eachRelease.release_name;
-    RELEASEOBJECT.thisReleaseDate = publishedAt.toDate();
-    RELEASEOBJECT.thisReleaseDay = publishedAt.format('D');
-    RELEASEOBJECT.thisReleaseMonth = publishedAt.format('MMMM');
-    RELEASEOBJECT.thisReleaseYear = publishedAt.format('YYYY');
-    RELEASEOBJECT.thisGitLink = eachRelease.release_link;
-    RELEASEOBJECT.thisDashLink = `https://dash.adoptopenjdk.net/version.html?version=${variant.replace('open','')}`
-      + `&tag=${encodeURIComponent(eachRelease.release_name)}`;
+    const release = {
+      release_name: aRelease.release_name,
+      release_link: aRelease.release_link,
+      dashboard_link: `https://dash.adoptopenjdk.net/version.html?version=${variant.replace('open','')}`
+        + `&tag=${encodeURIComponent(aRelease.release_name)}`,
 
-    // create an array of the details for each asset that is attached to this release
-    const assetArray = eachRelease.binaries;
+      release_day: publishedAt.format('D'),
+      release_month: publishedAt.format('MMMM'),
+      release_year: publishedAt.format('YYYY'),
+
+      platforms: {},
+    };
 
     // populate 'platformTableRows' with one row per binary for this release...
-    assetArray.forEach((eachAsset) => {
-      const ASSETOBJECT = {};
-      const uppercaseFilename = eachAsset.binary_name.toUpperCase(); // make the name of the asset uppercase
+    aRelease.binaries.forEach(aReleaseAsset => {
+      const platform = findPlatform(aReleaseAsset);
 
-      ASSETOBJECT.thisPlatform = findPlatform(eachAsset);
+      // Skip this asset if its platform could not be matched (see the website's 'config.json')
+      if (!platform) {
+        return;
+      }
 
-      // firstly, check if the platform name is recognised...
-      if (ASSETOBJECT.thisPlatform) {
-        // if the filename contains both the platform name and the matching INSTALLER extension, add the relevant info to the asset object
-        ASSETOBJECT.thisInstallerExtension = getInstallerExt(ASSETOBJECT.thisPlatform);
+      // Skip this asset if it's not a binary type we're interested in displaying
+      const binary_type = aReleaseAsset.binary_type.toUpperCase();
+      if (!['INSTALLER', 'JDK', 'JRE'].includes(binary_type)) {
+        return;
+      }
 
-        ASSETOBJECT.thisBinaryExtension = getBinaryExt(ASSETOBJECT.thisPlatform); // get the file extension associated with this platform
-
-        if (uppercaseFilename.includes(ASSETOBJECT.thisInstallerExtension.toUpperCase())) {
-          if (ASSETARRAY.length) {
-            ASSETARRAY.forEach((asset) => {
-              if (asset.thisPlatform === ASSETOBJECT.thisPlatform) {
-                ASSETARRAY.pop();
-              }
-            });
-          }
-          ASSETOBJECT.thisPlatformExists = true;
-          ASSETOBJECT.thisInstallerExists = true;
-          RELEASEOBJECT.installersExist = true;
-          ASSETOBJECT.thisInstallerLink = eachAsset.binary_link;
-          ASSETOBJECT.thisInstallerSize = Math.floor((eachAsset.binary_size) / 1024 / 1024);
-          ASSETOBJECT.thisOfficialName = getOfficialName(ASSETOBJECT.thisPlatform);
-          ASSETOBJECT.thisBinaryExists = true;
-          RELEASEOBJECT.binariesExist = true;
-          ASSETOBJECT.thisBinaryLink = eachAsset.binary_link.replace(ASSETOBJECT.thisInstallerExtension, ASSETOBJECT.thisBinaryExtension);
-          ASSETOBJECT.thisBinarySize = Math.floor((eachAsset.binary_size) / 1024 / 1024);
-          ASSETOBJECT.thisChecksumLink = eachAsset.checksum_link;
-
-          ASSETOBJECT.thisPlatformOrder = getPlatformOrder(ASSETOBJECT.thisPlatform);
-          if (Object.keys(jckJSON).length === 0) {
-            ASSETOBJECT.thisVerified = false;
-          } else {
-            if (jckJSON[eachRelease.release_name] && jckJSON[eachRelease.release_name].hasOwnProperty(ASSETOBJECT.thisPlatform)) {
-              ASSETOBJECT.thisVerified = true;
-            } else {
-              ASSETOBJECT.thisVerified = false;
-            }
-            ASSETOBJECT.thisPlatformOrder = getPlatformOrder(ASSETOBJECT.thisPlatform);
-          }
-        }
-
-        // secondly, check if the file has the expected file extension for that platform...
-        // (this filters out all non-binary attachments, e.g. SHA checksums - these contain the platform name, but are not binaries)
-
-        if (uppercaseFilename.includes(ASSETOBJECT.thisBinaryExtension.toUpperCase())) {
-          let installerExist = false;
-          if (ASSETARRAY.length) {
-            ASSETARRAY.forEach((asset) => {
-              if (asset.thisPlatform === ASSETOBJECT.thisPlatform) {
-                installerExist = true;
-              }
-            });
-          }
-
-          if (!installerExist) {
-            // set values ready to be injected into the HTML
-            ASSETOBJECT.thisPlatformExists = true;
-            ASSETOBJECT.thisBinaryExists = true;
-            RELEASEOBJECT.binariesExist = true;
-            ASSETOBJECT.thisOfficialName = getOfficialName(ASSETOBJECT.thisPlatform);
-            ASSETOBJECT.thisBinaryLink = (eachAsset.binary_link);
-            ASSETOBJECT.thisBinarySize = Math.floor((eachAsset.binary_size) / 1024 / 1024);
-            ASSETOBJECT.thisChecksumLink = eachAsset.checksum_link;
-            ASSETOBJECT.thisPlatformOrder = getPlatformOrder(ASSETOBJECT.thisPlatform);
-            if (Object.keys(jckJSON).length === 0) {
-              ASSETOBJECT.thisVerified = false;
-            } else {
-              if (jckJSON[eachRelease.release_name] && jckJSON[eachRelease.release_name].hasOwnProperty(ASSETOBJECT.thisPlatform)) {
-                ASSETOBJECT.thisVerified = true;
-              } else {
-                ASSETOBJECT.thisVerified = false;
-              }
-            }
-          }
-        }
-
-        if (ASSETOBJECT.thisPlatformExists) {
-          ASSETARRAY.push(ASSETOBJECT);
+      if (!release.platforms[platform]) {
+        release.platforms[platform] = {
+          official_name: getOfficialName(platform),
+          ordinal: getPlatformOrder(platform),
+          assets: [],
         }
       }
+
+      release.platforms[platform].assets.push({
+        type: binary_type,
+        extension: 'INSTALLER' === binary_type ? getInstallerExt(platform) : getBinaryExt(platform),
+        size: Math.floor(aReleaseAsset.binary_size / 1024 / 1024),
+
+        link: aReleaseAsset.binary_link,
+        checksum_link: aReleaseAsset.checksum_link,
+      });
     });
 
-    RELEASEOBJECT.thisPlatformAssets = orderPlatforms(ASSETARRAY);
-    RELEASEARRAY.push(RELEASEOBJECT);
-  }
+    Object.values(release.platforms).forEach(aPlatform => {
+      sortByProperty(aPlatform.assets, 'type');
+    });
 
-  // Sort releases by date/timestamp in descending order
-  RELEASEARRAY.sort((a, b) => b.thisReleaseDate - a.thisReleaseDate);
+    // Converts the `platforms` object to a sorted array
+    release.platforms = sortByProperty(release.platforms, 'ordinal');
+    releases.push(release);
+  });
+
+  // Sort releases by name in descending order.
+  // The release timestamp can't be relied upon due to out-of-order releases.
+  // Example: 'jdk8u191-b12' was released after 'jdk8u192-b12'
+  sortByProperty(releases, 'release_name', true);
 
   const template = Handlebars.compile(document.getElementById('template').innerHTML);
-  document.getElementById('archive-table-body').innerHTML = template({htmlTemplate: RELEASEARRAY});
+  document.getElementById('archive-table-body').innerHTML = template({releases});
 
   setPagination();
-  setTickLink();
 
   loading.innerHTML = ''; // remove the loading dots
 
