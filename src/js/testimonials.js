826a8e42ec3d7290e5b7fc7dd88701c600463e5b
George Adams <georgeadams1995@gmail.com>
2019-04-12 10:43:34 +0100

    more tweaks

--- src/js/testimonials.js
+++ src/js/testimonials.js
@@ -1,20 +1,27 @@
 module.exports.load = () => {
   showSlides(slideIndex);
 
-  const timer = setInterval(function() {
-    showSlides(slideIndex += 1);
-  }, 4000);
+  function myTimer() {
+    plusSlides(1)
+  }
 
-  $('.testimonials').hover(function() {
-    clearTimeout(timer)
-  });
+  let timer = setInterval(myTimer, 4000);
+
+  $(".quote").mouseenter(function() {
+    clearTimeout(timer);
+  })
+
+  $(".quote").mouseout(function() {
+    timer = setInterval(myTimer, 4000);
+  })
 }
 
 let slideIndex = 1;
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("testimonial");
+  let i;
+  const slides = document.getElementsByClassName("testimonial");
+  const dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1
   }
@@ -24,9 +31,17 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 global.plusSlides = function(n) {
   showSlides(slideIndex += n);
+}
+
+global.currentSlide = function(n) {
+  showSlides(slideIndex = n);
 }
