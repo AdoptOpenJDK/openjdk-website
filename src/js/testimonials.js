aaf45bc75ce0151ef70245cca8524f6aa00f95fb
George Adams <george@jclarity.com>
2019-04-16 21:03:51 +0100

    Add Testimonials page and Users page (#450)
    * testimonials page
    
    * more changes
    
    * more tidy fixes
    
    * more tweaks
    
    * add more users page
    
    * further fixes
    
    * remove unapproved logos for now
    
    * fix linter
    
    * fix godaddy logo
    
    * change bluemix to ibmcloud
    
    * fix background url
    
    * add more logos
    
    * fixes from review

---
+++ src/js/testimonials.js
@@ -0,0 +1,47 @@
+module.exports.load = () => {
+  showSlides(slideIndex);
+
+  function myTimer() {
+    global.plusSlides(1)
+  }
+
+  let timer = setInterval(myTimer, 4000);
+
+  $('.quote').mouseenter(function() {
+    clearTimeout(timer);
+  })
+
+  $('.quote').mouseout(function() {
+    timer = setInterval(myTimer, 4000);
+  })
+}
+
+let slideIndex = 1;
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName('testimonial');
+  const dots = document.getElementsByClassName('dot');
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+}
+
+global.plusSlides = function(n) {
+  showSlides(slideIndex += n);
+}
+
+global.currentSlide = function(n) {
+  showSlides(slideIndex = n);
+}
