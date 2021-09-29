2befb64a555e6350dc96bc02aed708b7bcd4ef6b
George Adams <georgeadams1995@gmail.com>
2021-09-29 12:21:27 +0100

    add further download redirections to home page

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,7 @@
 const {
   detectOS,
-  findPlatform,
   loadLatestAssets,
-  makeQueryString,
   setRadioSelectors,
-  setTickLink
 } = require('./common');
 const {
   jvmVariant,
@@ -16,10 +13,9 @@ const loading = document.getElementById('loading');
 const errorContainer = document.getElementById('error-container');
 const dlText = document.getElementById('dl-text');
 const dlLatest = document.getElementById('dl-latest');
+const dlLatestText = document.getElementById('dl-latest-text');
 const dlArchive = document.getElementById('dl-archive');
 const dlOther = document.getElementById('dl-other');
-const dlIcon = document.getElementById('dl-icon');
-const dlIcon2 = document.getElementById('dl-icon-2');
 const dlVersionText = document.getElementById('dl-version-text');
 
 // When index page loads, run:
@@ -35,11 +31,8 @@ module.exports.load = () => {
   }
   dlText.classList.remove('invisible');
 
-  const handleResponse = (releasesJson) => {
-    if (!releasesJson) {
-      return;
-    }
-    buildHomepageHTML(releasesJson, {}, OS);
+  const handleResponse = () => {
+    buildHomepageHTML(jvmVariant);
   };
 
   loadLatestAssets(variant, jvmVariant, 'latest', handleResponse, undefined, () => {
@@ -58,53 +51,15 @@ function removeRadioButtons() {
   }
 }
 
-function buildHomepageHTML(releasesJson, jckJSON, OS) {
-  let matchingFile = null;
-
-  // if the OS has been detected...
-  if (OS) {
-    releasesJson.forEach((eachAsset) => { // iterate through the assets attached to this release
-      const uppercaseFilename = eachAsset.binary.package.name.toUpperCase();
-      const thisPlatform = findPlatform(eachAsset.binary);
-
-      // firstly, check if a valid searchableName has been returned (i.e. the platform is recognised)...
-      if (thisPlatform) {
-        // secondly, check if the file has the expected file extension for that platform...
-        // (this filters out all non-binary attachments, e.g. SHA checksums - these contain the platform name, but are not binaries)
-        if (matchingFile == null) {
-          const uppercaseOSname = OS.searchableName.toUpperCase();
-          if (Object.keys(jckJSON).length !== 0) {
-            if (jckJSON[releasesJson.tag_name] && Object.prototype.hasOwnProperty.call(jckJSON[releasesJson.tag_name], uppercaseOSname)) {
-              document.getElementById('jck-approved-tick').classList.remove('hide');
-              setTickLink();
-            }
-          }
-          // thirdly check if JDK or JRE (we want to serve JDK by default)
-          if (eachAsset.binary.image_type == 'jdk') {
-            // fourthly, check if the user's OS searchableName string matches part of this binary's name (e.g. ...X64_LINUX...)
-            if (uppercaseFilename.includes(uppercaseOSname)) {
-              matchingFile = eachAsset; // set the matchingFile variable to the object containing this binary
-            }
-          }
-        }
-      }
-    });
-  }
-
-  // if there IS a matching binary for the user's OS...
-  if (matchingFile) {
-    if (matchingFile.binary.installer) {
-      dlLatest.href = matchingFile.binary.installer.link; // set the main download button's link to be the installer's download url
-    } else {
-      dlLatest.href = matchingFile.binary.package.link; // set the main download button's link to be the binary's download url
-      dlVersionText.innerHTML += ` - ${Math.floor(matchingFile.binary.package.size / 1000 / 1000)} MB`;
-    }
-    // set the download button's version number to the latest release
-    dlVersionText.innerHTML = matchingFile.release_name;
-  } else {
-    dlIcon.classList.add('hide'); // hide the download icon on the main button, to make it look less like you're going to get a download immediately
-    dlIcon2.classList.remove('hide'); // un-hide an arrow-right icon to show instead
-    dlLatest.href = `./releases.html?${makeQueryString({variant, jvmVariant})}`; // set the main download button's link to the latest releases page for all platforms.
+function buildHomepageHTML(jvmVariant) {
+  if (jvmVariant == 'hotspot') {
+    dlLatest.href = 'https://adoptium.net';
+    dlLatestText.textContent = 'adoptium.net';
+    dlVersionText.innerHTML = 'AdoptOpenJDK has moved...';
+  } else if (jvmVariant == 'openj9') {
+    dlLatest.href = 'https://developer.ibm.com/languages/java/semeru-runtimes/downloads';
+    dlLatestText.textContent = 'developer.ibm.com';
+    dlVersionText.innerHTML = 'AdoptOpenJDK has moved...';
   }
 
   // remove the loading dots, and make all buttons visible, with animated fade-in
