58e972067675f85f7c761f56af87ac19d743a56d
George Adams <georgeadams1995@gmail.com>
2021-09-29 12:48:14 +0100

    Update assemble.sh

--- assemble.sh
+++ assemble.sh
@@ -3,10 +3,10 @@ mv sitemap.xml tmp/
 find . -type f ! -name '*.html' ! -name '*.pdf' -maxdepth 1 -mindepth 1  -delete
 find . -type d -not -name 'dist' -not -name '.git' -maxdepth 1 -mindepth 1 -exec rm -rf {} \;
 # After this bulk-delete, copy across some other necessary files from the master branch:
-git checkout main -- NOTICE
-git checkout main -- LICENSE
+git checkout master -- NOTICE
+git checkout master -- LICENSE
 cp tmp/sitemap.xml .
-git checkout main -- robots.txt
-git checkout main -- CNAME
+git checkout master -- robots.txt
+git checkout master -- CNAME
 echo "These files are ready to be moved onto the production web server:"
 ls
